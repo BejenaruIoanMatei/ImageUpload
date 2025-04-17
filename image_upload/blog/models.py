dd9e0901a009d11b5064144669b875d32022b86d
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-17 13:38:28 +0300

    4. Post model new field: content

--- image_upload/blog/models.py
+++ image_upload/blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post_images/')
+    content = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.title
